90f3eab188ad49b472f23e651242668c66fb6689
James Harrington <jamesharrington585@hotmail.com>
2023-04-04 21:28:08 -0400

    projects now render! now for styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
-
+import avatar from '../assets/images/avatar2.png'
 
 function About() {
     return (
-        <div>
+        <section>
             <h2>About Me</h2>
-            {/* <img></img> */}
+            <img src={avatar} alt='stuff'/>
+            <div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum mi nec augue eleifend posuere. Fusce vitae pulvinar lectus, a laoreet purus. Nullam eleifend metus ac diam pretium varius. Phasellus luctus nunc non fermentum consequat. Etiam facilisis nunc in mi tincidunt mattis. Pellentesque nec lacinia ex. Nullam et ex egestas, porttitor massa eu, cursus erat. Integer sodales, erat at tempus lobortis, tellus mauris pretium neque, sit amet convallis nisl nulla a nibh. Suspendisse vulputate, mi sagittis varius finibus, erat lorem efficitur lorem, et condimentum diam lectus at metus. Donec ac sagittis turpis. Curabitur eu turpis sed justo hendrerit ullamcorper. Suspendisse eu magna sit amet ligula consectetur gravida nec nec enim. Nullam euismod fermentum metus elementum placerat. Mauris elementum convallis gravida.</p>
-        </div>
+            </div>
+        </section>
     );
 }
 
--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -1,9 +1,89 @@
+// import { useState } from 'react';
+
+import coffee from "../../assets/images/coffee.png";
+import frog from "../../assets/images/frog.jpg";
+import plant from "../../assets/images/plant.jpg";
+import trust from "../../assets/images/trust.jpg";
+import thing from "../../assets/images/untitled.png";
+
 function Project() {
-    return (
-        <div>
-            <h1>THESE ARE THE PROJECTS</h1>
+  const projects = [
+    {
+      id: 1,
+      name: "Zoom Bingo",
+      image: trust,
+      link: "https://mighty-refuge-69635.herokuapp.com/",
+    },
+    {
+      id: 2,
+      name: "Plant Measurer",
+      image: plant,
+      link: "https://google.com/",
+    },
+    {
+      id: 3,
+      name: "Coffee Management",
+      image: coffee,
+      link: "https://google.com/",
+    },
+    {
+      id: 4,
+      name: "Frog exaggerator",
+      image: frog,
+      link: "https://google.com/",
+    },
+    {
+      id: 5,
+      name: "The App",
+      image: thing,
+      link: "https://google.com/",
+    },
+  ];
+
+  // const [projects] = useState([
+  //     {
+  //       name: 'Zoom Bingo',
+  //       image: {trust},
+  //       link: 'https://mighty-refuge-69635.herokuapp.com/'
+  //     },
+  //     {
+  //       name: 'Plant Measurer',
+  //       image: {plant},
+  //       link: 'https://google.com/'
+  //     },
+  //     {
+  //       name: 'Coffee Management',
+  //       image: {coffee},
+  //       link: 'https://google.com/'
+  //     },
+  //     {
+  //       name: 'Frog exaggerator',
+  //       image: {frog},
+  //       link: 'https://google.com/'
+  //     },
+  //     {
+  //       name: 'The App',
+  //       image: {thing},
+  //       link: 'https://google.com/'
+  //     }
+  //   ]);
+
+  // const [currentProject, setCurrentProject] = useState(projects[0]);
+
+  // const handleProject = (project) => setCurrentProject(project);
+
+  return (
+    <div>
+      {projects.map((project) => (
+        <div key={project.id}>
+          <h3>{project.name}</h3>
+          <a href={project.link}>
+            <img src={project.image} alt="things" />
+          </a>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default Project;
